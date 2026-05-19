561c025cf4d36bee17c29c15a467a9e03dc71062
Justin <justinhartman08@gmail.com>
2026-05-19 10:19:14 -0700

    ok

--- unit_1.py
+++ unit_1.py
@@ -6,7 +6,18 @@ def registration():
     return username, password
 
 def save_user_data(username, password):
-    user_data = {'username': username, 'password': password}
-    with open('user_data.json', 'w') as f:
-        json.dump(user_data, f)
+    try:
+        with open('user_data.json', 'r') as f:
+            user_data = json.load(f)
+    except FileNotFoundError:
+        user_data = []
+               
+    user_data.append({'username': username, 'password': password})  
 
+    with open('user_data.json', 'w') as f:
+        json.dump(user_data, f, indent=2)
+    
+username, password = registration()
+if save_user_data(username, password):
+    print("User saved successfully!")
+    break
