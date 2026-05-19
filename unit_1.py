75ff5700a4454c39344e5200bde68e0a52d5e339
Justin <justinhartman08@gmail.com>
2026-05-19 08:52:14 -0700

    username/password registration

--- unit_1.py
+++ unit_1.py
@@ -1,1 +1,12 @@
-print('hello')
+import json
+
+def registration():
+    username = input('Please enter your username: ')
+    password = input('Please enter your password: ')
+    return username, password
+
+def save_user_data(username, password):
+    user_data = {'username': username, 'password': password}
+    with open('user_data.json', 'w') as f:
+        json.dump(user_data, f)
+
